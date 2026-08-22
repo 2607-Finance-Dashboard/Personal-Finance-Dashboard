39a857493efb5b14a10f8175b05f538a00e423ca
katanavenus@gmail.com <katanavenus@gmaiol.com>
2026-08-22 13:13:10 +0300

    complete side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
     <Navbar/>
-      
-    </>
-  )
+
+    <div className="flex">
+      <Sidebar/>
+
+      <main className='flex-1 p-6'>
+        <h1 className='text-2xl font-bold text-gray-900'>
+          Dashboard
+        </h1>
+      </main>
+    </div> 
+    </div>
+    
+  );
 }
 
 export default App
