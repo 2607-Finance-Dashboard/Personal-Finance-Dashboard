72e834caaa8f1b4c9d8fc16e9c0a9d816bdecd70
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-22 19:51:41 +0300

    add Transactions import from pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Transactions from './pages/Transactions'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-gray-50">
+      <Transactions />
+    </div>
   )
 }
 
