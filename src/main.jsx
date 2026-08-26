5a2bb9bdfc02047b162c6eee75a0664fa6700965
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-26 09:02:25 +0300

    fix main.jsx duplicates

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Analytics from "./components/analytics/Analytics";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+
+
 import './index.css'
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
